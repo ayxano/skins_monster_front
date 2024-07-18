c70eaa6e73effac8c080d96aebc3947e676e3895
dima <din.salikhov@gmail.com>
2024-07-18 18:50:48 +0300

    google tm

--- app.vue
+++ app.vue
@@ -26,7 +26,7 @@ import { useHead, useRoute } from "#app";
 import HeaderComponent from "~/components/header/index.vue";
 import FooterComponent from "~/components/footer/index.vue";
 import AsideComponent from "~/components/aside/index.vue";
-import { onMounted, computed } from "vue";
+import { computed } from "vue";
 import ModalsComponent from "~/components/modals/index.vue";
 import MenuComponent from "~/components/menus/index.vue";
 
@@ -38,6 +38,14 @@ useHead({
       name: "description",
       content:
         "Buy skins at a good price in Skins Monster, you can buy CS:GO/CS2/Dota 2 skins quickly and profitably. Skins market with a large number of CS:GO/CS2/Dota 2 items available for buy",
+    },
+  ],
+  noscript: [
+    {
+      innerHTML:
+        '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5K4VZCG7"\n' +
+        'height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+      tagPosition: "bodyOpen",
     },
   ],
 });
